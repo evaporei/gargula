d1b81eb8bab8769fda9034cea448267b6ea6df8a
Otávio Pace <otaviopp8@gmail.com>
2018-03-20 18:32:55 -0300

    release 1.0.2 :dragon:

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
+const path = require('path')
 
-const gargoyle = fs.readFileSync('gargoyle.ascii')
+const gargoyle = fs.readFileSync(path.join(__dirname, 'gargoyle.ascii'), 'utf8')
 
 module.exports = () => console.log(gargoyle.toString())
